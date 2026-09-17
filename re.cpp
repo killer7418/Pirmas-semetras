78d833af1c9624d3af42570c6ee30aedd2deb79b
killer7418 <109579957+killer7418@users.noreply.github.com>
2026-09-17 13:33:37 +0300

    Add files via upload

---
+++ re.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a=54;
+    int b=31;
+    int c=142;
+    int d=38;
+    int f=71;
+    int vid =(a+b+c+d+f)/5;
+    cout<<vid<<endl;
+
+    return 0;
+
+
+}
---
+++ tr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int a=13;
+    int b=5;
+    double c=17.5;
+
+    cout<<a+b-c<<endl;
+    cout<<15/2+c<<endl;
+    cout<<a/static_cast<double>(b)+2*c<<endl;
+    cout<<14%3+6.3+b/a<<endl;
+    cout<<static_cast<int>(c)%5+a-b<<endl;
+    cout<<13.5/2+4.0*3.5+18<<endl;
+
+
+
+    return 0;
+}
