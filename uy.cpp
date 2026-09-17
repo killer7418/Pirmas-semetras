3db9a4d3b7cfde84185175da3b92b6c1c6c2accb
killer7418 <109579957+killer7418@users.noreply.github.com>
2026-09-17 13:34:07 +0300

    Add files via upload

---
+++ uy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string marke = "Toyota";
+    string modelis = "Avensis";
+    int  metai = 2011;
+    double litrazas = 2.0;
+    string spalva = "Pilkos";
+
+    cout<<"Automobilis "<<marke<<" "<<modelis<<" yra pagamintas "<<metai<<" metais"<<".Jo motoras "<<litrazas<<" litrazo"<<" Automobilis yra "<<spalva<<" spalvos"<<endl;
+
+    return 0;
+
+
+
+}
