91c413c2368ac3b4ae7402c2efa7068f03ec75e1
killer7418 <109579957+killer7418@users.noreply.github.com>
2026-09-17 09:50:02 +0300

    Add files via upload

---
+++ pu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main () {
+
+    string klubas ="Vilniaus Rytas";
+    int metai ="1997";
+    string savininkas ="Jaroslav Latusinskij";
+    string arena ="Jeep arena";
+    int vietas ="2500";
+
+    cout<<klubas<<" "<<metai<<" "<<savininkas<<" "<<arena<<" "<<vietas<<endl;
+
+    return 0;
+
+
+
+
+}
---
+++ pu.h
@@ -0,0 +1,13 @@
+//
+// Created by s066413 on 9/17/2026.
+//
+
+#ifndef PIRMAS_SEM_PU_H
+#define PIRMAS_SEM_PU_H
+
+
+class pu {
+};
+
+
+#endif //PIRMAS_SEM_PU_H
