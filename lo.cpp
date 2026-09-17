c9db609bd344908d07c80543cb2e3f446be9c4a6
killer7418 <109579957+killer7418@users.noreply.github.com>
2026-09-17 09:29:02 +0300

    Add files via upload

---
+++ lo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string vardas="Jonas";
+    string pavarde = "Jonaitis";
+    int amzius = 20;
+    string grupe ="IT-200";
+    int kursas = 2;
+    string studija ="Informatika";
+
+    cout<<vardas  <<" "<<pavarde<<" "<<amzius<<" "<<grupe<<" "<<kursas<<" "<<studija<<endl;
+
+
+    return 0;
+
+}
